8895447c41e7e6b4ee45768f6651459edfe92373
Nileshlad-Zeus <nilesh.lad@zeuslearning.com>
2024-08-16 10:20:14 +0530

    RabbitMQ Producer consumer programm

--- Backend/RabbitMQ/Consumer/obj/Debug/net8.0/consumer.AssemblyInfo.cs
+++ Backend/RabbitMQ/Consumer/obj/Debug/net8.0/consumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("consumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b63aa1fbb938110a59cd3edbea6288e11a6e89b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac73274515c9f3d1ba10de433c44244f98c8c0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("consumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("consumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/RabbitMQ/Producer/Program.cs
+++ Backend/RabbitMQ/Producer/Program.cs
@@ -2,27 +2,16 @@
 using System.Text;
 using RabbitMQ.Client;
 
+var factory = new ConnectionFactory { HostName = "localhost" };
 
-//create new connection Factory
-var factory = new ConnectionFactory{HostName="localhost"};
-
-//new connection
 using var connection = factory.CreateConnection();
 
-//create new channels
 using var channel = connection.CreateModel();
-
-channel.QueueDeclare(queue:"letterbox", durable:false, exclusive:false, autoDelete:false, arguments:null);
-
-var message = "Nilesh";
-
-var encodeMessage = Encoding.UTF8.GetBytes(message);
-
-channel.BasicPublish("","letterbox", null, encodeMessage);
-
-Console.WriteLine($"Published message {message}");
-
-
-
-
-
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+for (var i = 0; i < 15; i++)
+{
+    var message = $"Nilesh {i}";
+    var encodeMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("", "letterbox", null, encodeMessage);
+    Console.WriteLine($"Published message {message} {i}");
+}
