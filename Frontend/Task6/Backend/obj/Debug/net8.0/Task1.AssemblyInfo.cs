1dcfc26b4fd5c142165118624dc0ddb72988dc9f
Nileshlad-Zeus <nilesh.lad@zeuslearning.com>
2024-08-16 10:24:00 +0530

    CRUD operations using minimal api .net

--- Frontend/Task6/Backend/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Frontend/Task6/Backend/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d72037050090fb78d2de2e3e696f203797ffab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac73274515c9f3d1ba10de433c44244f98c8c0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
